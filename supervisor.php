7747ef9c1dc9c61920fbf0fc006721ea3e983bc6
burasovartem <morti.show@gmail.com>
2020-06-06 04:02:54 +0500

    update

--- supervisor.php
+++ supervisor.php
@@ -1,7 +1,50 @@
+<?php require "./debug/db_link.php" ?>
 <html>
 	<?php require "components/head.htm" ?>
 	<body>
 		<?php require "components/header.htm" ?>
-		<?php require "components/supervisor-main.htm" ?>
+		<main>
+			<div class="current-tasks">
+				<div class="title">
+					Текущие задачи
+				</div>
+				<div class="list-container">
+					<div class="item">
+						Сделать мне кофе
+					</div>
+				</div>
+			</div>
+			<div class="documents">
+				<div class="check-title">
+					Документы
+				</div>
+				<div class="check-container">
+					<div class="check-item">
+						<input type="checkbox">
+						<p>Название документа</p>
+					</div>
+				</div>
+			</div>
+			<div class="intern-list">
+				<div class="title">
+					Стажеры
+				</div>
+				<div class="list-container">
+					<?php
+						$sql = "SELECT * FROM users";
+						$result = mysqli_query($link, $sql);
+						while ($row = mysqli_fetch_assoc($result)) {
+							$id = $row['id'];
+							$name = $row['name'];
+							echo "
+								<div class='item' id='$id'>
+									$name
+								</div>
+							";
+						}
+					?>
+				</div>
+			</div>
+		</main>
 	</body>
 </html>
