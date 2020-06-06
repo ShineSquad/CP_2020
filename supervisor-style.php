442d35d93b70a2aa6fa9eaee221d8a7f4db4801b
burasovartem <morti.show@gmail.com>
2020-06-07 03:50:30 +0500

    clean

--- supervisor-style.php
+++ supervisor-style.php
@@ -1,191 +1,152 @@
-<?php
+<?php 
 	require "./debug/db_link.php";
-	$user_type = 3;
+	$user_type = 2;
 ?>
-<!DOCTYPE html>
 <html>
-<head>
-	<meta charset="utf-8">
-	<title>Главная</title>
-	<link rel="stylesheet" type="text/css" href="styles/style.css">
-	<script type="text/javascript" src="scripts/script.js"></script>
-	<script type="text/javascript">
-		function open_reglament() {
-			var main = document.getElementById('main-content'),
-				generalities = document.getElementById('generalities');
-			main.style.display = "none";
-			generalities.style.display = "block";
-		}
-
-		function back() {
-			var main = document.getElementById('main-content'),
-				generalities = document.getElementById('generalities'),
-				tasks = document.getElementById('tasks');
-			main.style.display = "block";
-			generalities.style.display = "none";
-			tasks.style.display = "none";
-		}
-
-		function open_task() {
-			var main = document.getElementById('main-content'),
-				tasks = document.getElementById('tasks');
-			main.style.display = "none";
-			tasks.style.display = "block";
-		}
-
-		function change_select() {
-			var view = document.getElementById("view"),
-				change = view.getAttribute("value");
-			if (change) {
-				view.style.height = "30px";
-				view.setAttribute("value", true);
-			} else {
-				view.style.height = "0px";
-				view.setAttribute("value", false);
-			}
-
-		}
-	</script>
-</head>
-<body>
-	<?php require "./components/header-style.htm"?>
-	<main>
-		<?php require "./components/card-style.htm"?>
-		<div class="title-instruction">
-			Инструкция должности
-		</div>
-		<div class="main-content" id="main-content">
-			<div class="top-content">
-				<div class="main-regulations" onclick="open_reglament()">
-					<div class="card-img">
-						<img src="styles/icons/4.png">
-					</div>
-					<div class="card-title">
-						<p>
-							Общие <br>
-							положения
-						</p>
-					</div>
-				</div>
-				<div class="active-tasks" onclick="open_task()">
-					<div class="card-img">
-						<img src="styles/icons/1.png">
-					</div>
-					<div class="card-title">
-						<p>
-							Задания для ознакомления с должностью
-						</p>
-					</div>
-				</div>
-			</div>
-			<div class="bottom-content">
-				<div class="job-responsibilities">
-					<div class="card-img">
-						<img src="styles/icons/2.png">
-					</div>
-					<div class="card-title">
-						<p>
-							Должностные обязанности, права и ответственность
-						</p>
-					</div>
-				</div>
-				<div class="documents">
-					<div class="card-img">
-						<img src="styles/icons/3.png">
-					</div>
-					<div class="card-title">
-						<p>
-							Документация <br>
-							и условия труда
-						</p>
-					</div>
-				</div>
-				<div class="institute-structure">
-					<div class="card-img">
-						<img src="styles/icons/5.png">
-					</div>
-					<div class="card-title">
-						<p>
-							Структура <br>
-							института
-						</p>
-					</div>
-				</div>
-			</div>
-		</div>
-		
-		<div class="generalities-content" id='generalities'>
-			<div class="generalities-left">
-				<img src="./styles/icons/arrow-back.svg" class="arrow-back" onclick="back()">
-				<div class="generalities-left-content">
-					<p>1. Педагог-психолог относится к категории специалистов.</p>
-					<p>2. На должность педагога-психолога назначается лицо, имеющее среднее психологическое или среднее педагогическое образование с дополнительной специальностью "Психология".</p>
-					<p>3. Назначение на должность педагога-психолога и освобождение производится приказом директора.</p>
-					<p>
-						4. Педагог-психолог должен знать: <br>
-						4.1. Конституцию РФ. <br>
-						4.5. Нормативные документы, регулирующие вопросы охраны труда, профориентации, занятости обучающихся (воспитанников) и их социальной защиты.
-					</p>
-				</div>
-			</div>
-			<div class="generalities-right">
-				<div class="generalities-right-content">
-					<p class="generalities-title">Руководители:</p>
-					<div class="list-description">
-						<li>Александров Макар Андреевич - Доктор наук химии</li>
-						<li>Брежнев Леонид Ильич - Доктор наук химии</li>
-					</div>
-				</div>
-				<div class="generalities-right-content">
-					<p class="generalities-title">Дрес-код:</p>
-					<div class="list-description">
-						<li>Описание одежды. На должность педагога-психолога назначается лицо.</li>
-						<li>Описание одежды.</li>
-					</div>
-				</div>
-				<div class="generalities-right-content">
-					<p class="generalities-title">Расписание:</p>
-					<div class="list-description">
-						<li>Описание тайминга. Обед или расписание работы или вообще, что угодно.</li>
-						<li>Текст</li>
-					</div>
-				</div>
-			</div>
-		</div>
-
-		<div class="tasks-content" id="tasks">
-			<div class="tasks-left">
-				<img src="./styles/icons/arrow-back.svg" class="arrow-back" onclick="back()">
-				<div class="task-title">
+	<?php require "components/head.htm" ?>
+	<body>
+		<?php require "components/header.htm" ?>
+		<main>
+			<div class="current-tasks">
+				<div class="title">
 					Задачи
 				</div>
-				<div class="task-list">
-					<div class="task-item">
-						Название
-					</div>
+				<div class="list-container">
+					<?php
+						$sql = "SELECT * FROM tasks WHERE status=0";
+						$result = mysqli_query($link, $sql);
+						while ($row = mysqli_fetch_assoc($result)) {
+							$id = $row['id'];
+							$title = $row['title'];
+							$node_id="n_".$id;
+							echo "
+								<label for='$node_id' class='item' id='$id'>
+									$title
+									<input id='$node_id' type='radio' name='task' value='$id'/>
+								</label>
+							";
+						}
+					?>
 				</div>
 			</div>
-			<div class="tasks-right">
-				<div class="tasks-right-content">
-					<div class="active-title">
-						Активные задачи
-					</div>
-					<div class="active-list">
-
-						<div class="active-item">
-							<div class="item-title">Название</div>
-							<div class="item-description">Описание Описание Описание ОписаниеОписаниеОписаниеОписание Описание Описание Описание Описание</div>
-							<div class="item-documents">
-								<a href="#" download="">Документ 1</a>
-								<a href="#" download="">Документ 2</a>
-							</div>
-						</div>
-
+			<div class="documents">
+				<div class="check-title">
+					<div class="title-name">
+						Документы
 					</div>
 				</div>
+				<div class="check-container">
+					<?php
+						$position = 1;
+						if (isset($_GET["change_position"])) {
+							$position = $_GET["position"];
+						}
+						if (isset($_GET["change_docs"])) {
+							$intern_id = $_GET["intern_id"];
+
+							$sql = "SELECT * FROM users_position WHERE user_id=$intern_id";
+							$result = mysqli_query($link, $sql);
+
+							while ($row = mysqli_fetch_assoc($result)) {
+								$position = $row["position_id"];
+							}
+						}
+
+							$sql = "SELECT * FROM positions WHERE id=$position";
+							$result = mysqli_query($link, $sql);
+							if ($result) {
+								while ($row = mysqli_fetch_assoc($result)) {
+									$pos_name = $row["name"];
+								}
+								// echo $pos_name;
+							}
+							
+						$sql = "SELECT instructions.* FROM instructions
+								INNER JOIN position_instructions
+								ON instructions.id = position_instructions.instruction_id
+								WHERE position_instructions.position_id = $position";
+						$result = mysqli_query($link, $sql);
+						while ($row = mysqli_fetch_assoc($result)) {
+							$id = $row['id'];
+							$name = $row['name'];
+							$node_id="m_".$id;
+							echo "
+								<label for='$node_id' class='check-item' id='$id'>
+									<input id='$node_id' type='checkbox' name='document'  value='$id'>
+									<p>$name</p>
+								</label>
+							";
+						}
+					?>
+				</div>
 			</div>
-		</div>
-	</main>
-	<?php require"./components/footer-style.htm"?>
-</body>
-</html> 
+			<div class="intern-list">
+				<div class="title">
+					Стажеры
+				</div>
+				<div class="list-container">
+					<?php
+						$sql = "SELECT * FROM users
+								INNER JOIN menthors ON users.id=menthors.supervisor WHERE menthors.supervisor=$user_id";
+						$interns = array();
+						$result = mysqli_query($link, $sql);
+						if ($result) {
+							while ($row = mysqli_fetch_assoc($result)) {
+								$interns[] = $row["intern"]; }
+						}
+						
+						$sql = "SELECT * FROM users WHERE id IN (" . implode(",", $interns) . ")";
+						$result = mysqli_query($link, $sql);
+
+						if ($result) {
+							while ($row = mysqli_fetch_assoc($result)) {
+								$id = $row['id'];
+								$name = $row['name'];
+								$node_id="f_".$id;
+								$selected = "";
+								if (isset($intern_id) && $intern_id == $id) $selected = "checked";
+
+								echo "
+									<label for='$node_id' class='item' id='$id'>
+										$name
+										<input id='$node_id' type='radio' name='intern' value='$id'
+										oninput='change_docs($id)'
+										$selected />
+									</label>
+								";
+							}
+						}
+					?>
+				</div>
+			</div>
+		</main>
+		<footer>
+			<button onclick="apply_task(); return false;">Выдать задание</button>
+		</footer>
+	</body>
+</html>
+
+<?php
+// http://localhost/CP_2020/supervisor.php?apply_task=1&task=4&d_1=1&d_2=2&intern=4
+	if (isset($_GET["apply_task"])) {
+		$task = $_GET["task"];
+		$intern = $_GET["intern"];
+
+		$sql = "UPDATE tasks SET from_id=$user_id, to_id=$intern, status=1 WHERE id=$task";
+		mysqli_query($link, $sql);
+
+		$sql = "INSERT INTO task_instructions (id, task_id, instruction_id)
+				VALUES ";
+
+		$values = array();
+		foreach ($_GET as $key => $value) {
+			if (preg_match("/d_/", $key)) {
+				$values[] = "(NULL, $task, $value)";
+			}
+		}
+		
+		$sql .= implode(",", $values);
+		// echo $sql;
+		mysqli_query($link, $sql);
+	}
+?>
